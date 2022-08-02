bd15ba2537b0430cd5640cac86197de733118dfd
Simrah Azfar <simrahazfar2@gmail.com>
2022-08-02 14:13:10 -0400

    fixed login css

--- src/components/Login.js
+++ src/components/Login.js
@@ -22,7 +22,7 @@ export default function (props) {
               </span>
             </div>
             <div className="form-group mt-3">
-              <label>Email address</label>
+              <label>Email Address:</label>
               <input
                 type="email"
                 className="form-control mt-1"
@@ -30,7 +30,7 @@ export default function (props) {
               />
             </div>
             <div className="form-group mt-3">
-              <label>Password</label>
+              <label>Password:</label>
               <input
                 type="password"
                 className="form-control mt-1"
