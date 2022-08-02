1e3b19821bfe2911013439868be7c14e59f71708
Masood Ashrafi <masood.ashrafi17@gmail.com>
2022-08-02 15:19:38 -0400

    fixed about page import

--- src/screens/AboutScreen.js
+++ src/screens/AboutScreen.js
@@ -1,6 +1,6 @@
 // import React from 'react'
 // import Navbar from '../components/Navbar';
-// import CheckoutProduct from '../components/About';
+// import About from '../components/About';
 // import Footer from '../components/Footer';
 
 // const AboutScreen = () => {
